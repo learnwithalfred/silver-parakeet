ccf03d3defbe5fdfec862f9565ca2bbbf5a46ccd
Alfred Boateng <kwadwo.boateng.alfred@gmail.com>
2022-09-29 11:47:14 +0000

    Update files

--- my_list.rb
+++ my_list.rb
@@ -14,12 +14,6 @@ end
 
 list = MyList.new(1, 2, 3, 4)
 
-puts list.all? { |e| e < 5 }
+list.all? { |e| e < 5 }
 
-puts list.all? { |e| e > 5 }
-
-puts list.any? {|e| e == 2}
-
-puts list.any? {|e| e == 5}
-
-print list.filter {|e| e.even?}
+list.all? { |e| e > 5 }
